58f818f92aa0cae910f707808866f17a7f0cd246
Yan <yangabriel2012@gmail.com>
2023-02-13 22:42:00 -0300

    feat: chamando a api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,28 @@ import profileImage from '../../public/images/profile.jpeg'
 import { ProfileImage } from '@/components/ProfileImage'
 import { PostContainer } from '@/components/PostContainer'
 import { useEffect } from 'react'
-import { usePostsContext } from './context/postsContext'
+import { usePostsContext } from '../context/postsContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const {title} = usePostsContext()
- console.log(title)
+  const callPosts = async () => {
+    const response = fetch('/api/posts')
+    const data = (await response).json()
+    const posts = data.then(p => console.log(p))
+   
+  }
+  useEffect(() => {
+    callPosts()
+  },[])
+
   return (
    <Content>
     <div className='flex flex-col items-center'>
     <Wallpaper image={wallpaperImage} />
     <ProfileImage image={profileImage}/>
     </div>
-    <PostContainer title='Title' prev='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore iure placeat soluta libero vel, pariatur quidem officiis quas expedita dignissimos beatae inventore praesentium quia deserunt earum quae recusandae obcaecati voluptatem.
+    <PostContainer titlee='Title' prev='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore iure placeat soluta libero vel, pariatur quidem officiis quas expedita dignissimos beatae inventore praesentium quia deserunt earum quae recusandae obcaecati voluptatem.
 '/>
    </Content>
     )
