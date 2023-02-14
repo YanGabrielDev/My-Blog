85fb55c132ae67ddbaacdf1dd6f4165b786da5b9
Yan <yangabriel2012@gmail.com>
2023-02-13 22:42:22 -0300

    testando a api

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -1,12 +1,20 @@
+import { useEffect } from "react"
+import { usePostsContext } from "../../context/postsContext"
+
 interface PostContainerProps{
-    title: string,
+    titlee: string,
     prev: string
 }
-export const PostContainer = ({prev, title}: PostContainerProps) => {
+export const PostContainer = ({prev, titlee}: PostContainerProps) => {
+    const {title, update} = usePostsContext()
+    useEffect(() => {
+    update('teste')
+    }, [])
+   console.log(title)
     return(
         <div className={`post-container w-full sm:w-9/12 rounded-2xl 
         bg-[#2B2A2A] px-4 m-0 sm:m-auto my-0 mb-4`}>   
-         <p className="p-2 text-white text-2xl">{title}</p>
+         <p className="p-2 text-white text-2xl">{titlee}</p>
          <hr/>
          <p className="p-2 text-white text-lg">{prev}</p>
         </div>
