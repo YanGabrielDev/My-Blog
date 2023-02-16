6a28a24c379c3664c1bcb0fc5e1067ec20a7cb39
Yan <yangabriel2012@gmail.com>
2023-02-16 20:45:51 -0300

    atualização do espaçamentos

--- src/components/Wallpaper/index.tsx
+++ src/components/Wallpaper/index.tsx
@@ -6,7 +6,7 @@ interface WallpaperProps{
 
 export const Wallpaper = ({image}:WallpaperProps) => {
     return(
-        <div className="w-full h-64 overflow-hidden relative mt-8 rounded-[14px]">
+        <div className="w-full h-48 overflow-hidden relative mt-8 rounded-[14px]">
             <Image src={image} alt="forest" className="absolute rounded-2xl wallpaper h-full object-cover"/>
         </div>
     )
