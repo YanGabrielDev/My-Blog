191c277d39435f2cb242367c3062dcc861883283
Yan <yangabriel2012@gmail.com>
2023-02-20 12:53:31 -0300

    adicionando revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { Inter } from '@next/font/google'
 import { Content } from '@/components/content'
 import { Wallpaper } from '@/components/Wallpaper'
 import wallpaperImage from '../../public/images/wallpaper.jpg'
@@ -7,32 +6,42 @@ import profileImage from '../../public/images/profile.jpeg'
 import { ProfileImage } from '@/components/ProfileImage'
 import { PostContainer } from '@/components/PostContainer'
 import { useEffect } from 'react'
-import { usePostsContext } from '../context/postsContext'
-
-const inter = Inter({ subsets: ['latin'] })
+import { usePostsContext } from '../context/PostContext/postsContext'
+import logo from '../../public/assets/icons/logo.svg'
+import Image from 'next/image'
 
 export default function Home() {
-  const {title} = usePostsContext()
+  const { update, title} = usePostsContext()
   const callPosts = async () => {
-    const response = fetch('/api/posts')
+    const response = fetch('/api/posts', {
+      next: {
+        revalidate: 60
+      }
+    })
     const data = (await response).json()
-    const posts = data.then(p => console.log(p))
+    const posts = data.then(p => update(p))
    
   }
   useEffect(() => {
     callPosts()
   },[])
-
+  console.log(title)
   return (
    <Content>
     <div className='flex flex-col items-center'>
+    {/* <Image src={logo} alt="logo"/> */}
     <Wallpaper image={wallpaperImage} />
     <ProfileImage image={profileImage}/>
     </div>
-    <PostContainer titlee={title} prev='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore iure placeat soluta libero vel, pariatur quidem officiis quas expedita dignissimos beatae inventore praesentium quia deserunt earum quae recusandae obcaecati voluptatem.
-'/>
-    <PostContainer titlee='Title' prev='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore iure placeat soluta libero vel, pariatur quidem officiis quas expedita dignissimos beatae inventore praesentium quia deserunt earum quae recusandae obcaecati voluptatem.
-'/>
+    {title.map((post:any) => {
+      return (
+        <>
+        <PostContainer title={post.title} 
+        prev={post.post}/>
+    </>
+      )
+    })}
+ 
    </Content>
     )
 }
