072d6863ab176332582568c3c9c2d2b52e85c0e8
Yan <yangabriel2012@gmail.com>
2023-02-20 12:53:55 -0300

    criando o component de button

---
+++ src/components/ModalImage/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+import closeIcon from "../../../public/assets/icons/close.svg";
+import { ButtonIcon } from "../ButtonIcon";
+interface ModalImageProps {
+  image: string;
+  alt: string;
+  close: () => void;
+}
+export const ModalImage = ({ image, alt, close }: ModalImageProps) => {
+  return (
+    <div
+      className={`fixed w-full h-full bg-opacity-80 bg-black flex justify-center items-center z-10 top-0 left-0`}
+    >
+      <Image alt={alt} src={image} className="max-w-xl"/>
+      <ButtonIcon icon={closeIcon} altIcon='close' onClick={() => close()} className="top-2 right-4 absolute"/>
+    </div>
+  );
+};
