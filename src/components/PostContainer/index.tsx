804a904231457319a6842f7c71f3a31e094a6f25
Yan <yangabriel2012@gmail.com>
2023-02-20 12:54:11 -0300

    limpando o codigo

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -1,20 +1,14 @@
-import { useEffect } from "react"
-import { usePostsContext } from "../../context/postsContext"
-
 interface PostContainerProps{
-    titlee: string,
+    title: string,
     prev: string
 }
-export const PostContainer = ({prev, titlee}: PostContainerProps) => {
-    const {title, update} = usePostsContext()
-    useEffect(() => {
-    update('teste')
-    }, [])
+export const PostContainer = ({prev, title}: PostContainerProps) => {
+
     return(
         <>
         <div className={`post-container w-full sm:w-9/12 rounded-2xl 
         bg-transparent mb-8`}>   
-         <p className="p-2 text-blue-500 text-2xl hover:underline">{titlee}</p>
+         <p className="p-2 text-blue-500 text-2xl hover:underline">{title}</p>
 
          <p className="p-2 text-white text-lg">{prev}</p>
         </div>
