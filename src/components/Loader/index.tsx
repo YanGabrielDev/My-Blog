091c1ed4f00dd1322699ff96bf429df181597077
Yan <yangabriel2012@gmail.com>
2023-02-21 10:38:45 -0300

    adicionando loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+    return(
+        <div className="w-full bg-[#151515] flex justify-center items-center">
+            <span className="loader"></span>
+        </div>
+    )
+}
