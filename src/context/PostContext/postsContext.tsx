6d9b1719f47e0d19bddddab0f5c6acf6c1300958
Yan <yangabriel2012@gmail.com>
2023-02-22 11:06:27 -0300

    implementando novos estados no context

--- src/context/PostContext/postsContext.tsx
+++ src/context/PostContext/postsContext.tsx
@@ -3,7 +3,9 @@ import { Posts } from "@/interfaces/postsInterface";
 import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
 interface PostContext{
     posts: Array<Posts>,
-    update: (c: Array<Posts>) => void
+    update: (c: Array<Posts>) => void,
+    selectedPost: Posts,
+    showSelectPost: (c: Posts) => void,
 }
 
 interface PostProvider{
@@ -11,17 +13,28 @@ interface PostProvider{
 }
 const PostContext = createContext<PostContext>({
       posts: [],
-      update: () => []
+      update: () => [],
+      showSelectPost: () => [],
+      selectedPost: {id: '', post: '', title: ''}
+
+
 })
 
 export const PostProvider =  ({children}: PostProvider) => {
 
     const [posts, setPosts] = useState<Array<Posts>>([])
+    const [selectedPost, setSelectedPost] = useState<Posts>({
+        id: '', post: '', title: ''
+    })
     const update = (c: Array<Posts>) => {
         setPosts(c)
     }
+
+    const showSelectPost = (c: Posts) => {
+        setSelectedPost(c)
+    }
     return(
-        <PostContext.Provider value={{posts, update}}>{children}</PostContext.Provider>
+        <PostContext.Provider value={{posts, update, showSelectPost, selectedPost}}>{children}</PostContext.Provider>
     )
 }
 export const usePostsContext = () => useContext(PostContext)
