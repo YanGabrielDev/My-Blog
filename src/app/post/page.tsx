1a92aba55c7c6a082eda0da234b07e0f6751dd2b
Yan <yangabriel2012@gmail.com>
2023-02-22 22:12:43 -0300

    implementação da pagina de post

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,25 @@
 "use client"
 
+import { Content } from "@/components/content"
+import {usePostsContext} from '../../context/PostContext/postsContext'
 export default function Post (){
+    const {selectedPost} = usePostsContext()
+    const {id, post, title} = selectedPost
+    const brakline = post.split('\n')
     return(
-        <h1>hff</h1>
+        <Content>
+            <div className="flex items-center w-full h-full flex-col pt-32">
+            <h1 className="text-blue-500 text-2xl">{title}</h1>
+            <div>
+            {brakline.map((line, index) => {
+                return(
+                    <>
+                    <span className="text-white mt-4" key={index}>{line}</span>
+                    </>
+                )
+            })}
+            </div>
+            </div>
+        </Content>
     )
 }
