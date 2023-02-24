7ed46d7aedabe175a1f8b7f4fbe7daddc062940f
Yan <yangabriel2012@gmail.com>
2023-02-23 22:41:47 -0300

    teste

--- src/context/PostContext/postsContext.tsx
+++ src/context/PostContext/postsContext.tsx
@@ -31,7 +31,7 @@ export const PostProvider =  ({children}: PostProvider) => {
     }
 
     const showSelectPost = (c: Posts) => {
-        setSelectedPost((c) => c)
+        setSelectedPost(c)
     }
     return(
         <PostContext.Provider value={{posts, update, showSelectPost, selectedPost}}>{children}</PostContext.Provider>
