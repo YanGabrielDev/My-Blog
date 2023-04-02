61d5bcb1dc93efb8db8b4fa5c8d6aae06bcac526
Yan <yangabriel2012@gmail.com>
2023-04-02 11:57:53 -0300

    fix: alterando o posicionamento

--- src/components/ProfileImage/index.tsx
+++ src/components/ProfileImage/index.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import { Roboto, Raleway } from "@next/font/google";
+import { Inter } from "@next/font/google";
 import { useState } from "react";
 import { ModalImage } from "../ModalImage";
 
 interface ProfileImageProps {
   image: string;
 }
-const relaway = Raleway({
-  weight: ["400", "700"],
+const inter = Inter({
+  weight: [ "500"],
   subsets: ["latin"],
   display: "swap",
 });
@@ -17,7 +17,7 @@ export const ProfileImage = ({ image }: ProfileImageProps) => {
   
   return (
     <div
-      className={`relative bottom-20 text-center flex flex-col items-center`}
+      className={`relative bottom-40 ml-8 text-center sm:flex`}
     >
       <div
         className={`h-[150px] w-[150px]  rounded-full bg-black flex justify-center items-center`}
@@ -30,10 +30,12 @@ export const ProfileImage = ({ image }: ProfileImageProps) => {
         />
         {openModal && <ModalImage image={image} alt="profile" close={onClose} />}
       </div>
-      <h3 className={`${relaway.className} text-white`}>Yan Gabriel</h3>
-      <p className={`text-[#909090] ${relaway.className}`}>
+      <div className=" flex flex-col sm:justify-center sm:ml-4 items-start sm:mt-0 mt-2">
+      <h1 id="profile-name" className={`${inter.className} text-white text-5xl font-sans`}>Yan Gabriel</h1>
+      <p className={`text-[#909090] ${inter.className} ml-[8px]`}>
         Front-end developer
       </p>
+      </div>
     </div>
   );
 };
