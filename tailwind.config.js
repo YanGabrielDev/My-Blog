9277938e7adb5dcf84f3bf41320df58131114d92
Yan <yangabriel2012@gmail.com>
2023-04-02 11:57:17 -0300

    feat: adicionando os breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'custom': '900px', // novo breakpoint personalizado
+    },
     // colors:{
     // opacityBlack: '0,0,0,0.8'
     // },
