6e700d10280ee7e4734fb1d3199f6c9f7a5e5071
Yan <yangabriel2012@gmail.com>
2023-04-02 11:57:46 -0300

    fix: alterando o posicionamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
   return (
     <>
     <Content>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col">
         <Wallpaper image={wallpaperImage} />
         <ProfileImage image={profileImage} />
       </div>
