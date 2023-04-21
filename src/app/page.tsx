148d6faba6d2f3c609e9ae98b828250aff56d418
Yan <yangabriel2012@gmail.com>
2023-04-21 18:28:22 -0300

    fix: corrigindo comportamento do walppaper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,44 +10,50 @@ import { usePostsContext } from "../context/PostContext/postsContext";
 import { Loader } from "@/components/Loader";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  const { update, posts, showSelectPost} = usePostsContext();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { update, posts, showSelectPost } = usePostsContext();
   const callPosts = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     const response = fetch("/api/posts", {
       next: {
-        
         revalidate: 60,
       },
     });
     const data = (await response).json();
     const posts = data.then((p) => update(p));
-    setIsLoading(false)
+    setIsLoading(false);
   };
   useEffect(() => {
-    try{
+    try {
       callPosts();
-    } catch(error){
-      
-      console.error(error)
+    } catch (error) {
+      console.error(error);
     }
   }, []);
   return (
     <>
-    <Content>
-      <div className="flex flex-col">
-        <Wallpaper image={wallpaperImage} />
-        <ProfileImage image={profileImage} />
-      </div>
-     {isLoading && <Loader/>}
-      {posts.map((post) => {
-        return (
-          <>
-            <PostContainer key={post.id} title={post.title} prev={post.preview} query={post.id} />
-          </>
-        );
-      })}
-    </Content>
+      <Content>
+        <div className="flex flex-col">
+          <Wallpaper image={wallpaperImage}>
+            <ProfileImage image={profileImage} />
+          </Wallpaper>
+        </div>
+        {isLoading && <Loader />}
+        <div className="mt-12">
+          {posts.map((post) => {
+            return (
+              <>
+                <PostContainer
+                  key={post.id}
+                  title={post.title}
+                  prev={post.preview}
+                  query={post.id}
+                />
+              </>
+            );
+          })}
+        </div>
+      </Content>
     </>
   );
 }
