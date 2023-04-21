9be4b62f0bac767f31da90d57a4723e55e84304c
Yan <yangabriel2012@gmail.com>
2023-04-21 18:29:38 -0300

    fix: ajustando posicionamento do profile

--- src/components/Wallpaper/index.tsx
+++ src/components/Wallpaper/index.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { ModalImage } from "../ModalImage";
 
 interface WallpaperProps {
   image: string;
+  children: ReactNode
 }
 
-export const Wallpaper = ({ image }: WallpaperProps) => {
+export const Wallpaper = ({ image, children }: WallpaperProps) => {
   const [openModal, setOpenModal] = useState<boolean>(false);
   const onClose = () => setOpenModal(false);
 
   return (
-    <div className="w-full h-56 overflow-hidden relative mt-12 rounded-[14px]">
+    <div className="w-full h-[16rem] overflow-hidden relative mt-16 rounded-[14px] flex flex-col justify-center">
       <Image
         src={image}
         alt="forest"
         className={`absolute rounded-2xl wallpaper h-full object-cover cursor-pointer`}
         onClick={() => setOpenModal(true)}
       />
+      {children}
       {openModal && (
         <ModalImage alt="wallpaper" close={onClose} image={image} />
       )}
