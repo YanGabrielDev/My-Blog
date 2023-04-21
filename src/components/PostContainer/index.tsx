909dc9d3c010cd8515fa1118ecb5b8f9d724eae9
Yan <yangabriel2012@gmail.com>
2023-04-21 18:28:56 -0300

    fix: deixando o codigo semantico

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -9,22 +9,23 @@ interface PostContainerProps {
 export const PostContainer = ({ prev, title, onClick, query}: PostContainerProps) => {
   return (
     <>
-      <div
+      <article
         className={`post-container w-full sm:w-9/12 rounded-2xl 
         bg-transparent mb-8`}
       >
+        <h2 className="p-2 text-blue-500 text-2xl hover:underline">
         <Link
           href={{ pathname: "/post", query: {id: query}}}
-          className="p-2 text-blue-500 text-2xl hover:underline"
           onClick={onClick}
           style={{
             lineHeight: "0px"
           }}
         >
-          <h2>{title}</h2>
+          {title}
         </Link>
+        </h2>
         <p className="p-2 text-white text-lg">{prev}</p>
-      </div>
+      </article>
     </>
   );
 };
